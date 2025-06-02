37ee1b7100acc1f2d742530ac6d2486984d50e5c
Nick Everitt <nicholaseveritt@gmail.com>
2025-06-02 20:09:51 +0100

    Fix indentation

--- Sources/StarField/NamesView.swift
+++ Sources/StarField/NamesView.swift
@@ -7,25 +7,25 @@ extension StarField {
         @ObservedObject var layout: Layout
 
         var body: some View {
-                let nameFont = configuration.nameFont
-                let nameColor = configuration.colorScheme.nameColor
+            let nameFont = configuration.nameFont
+            let nameColor = configuration.colorScheme.nameColor
 
-                Canvas { context, size in
-                    let resolver: TextResolver = { string in
-                        let text = Text(verbatim: string)
-                            .font(nameFont)
-                            .foregroundStyle(nameColor)
+            Canvas { context, size in
+                let resolver: TextResolver = { string in
+                    let text = Text(verbatim: string)
+                        .font(nameFont)
+                        .foregroundStyle(nameColor)
 
-                        return context.resolve(text)
-                    }
+                    return context.resolve(text)
+                }
 
-                    let namesArtist = StarField.GraphicsArtist(
-                        context: context,
-                        colorScheme: configuration.colorScheme)
+                let namesArtist = StarField.GraphicsArtist(
+                    context: context,
+                    colorScheme: configuration.colorScheme)
 
-                    let nameGraphics = layout.layoutNames(using: resolver)
-                    nameGraphics.forEach { graphic in
-                        namesArtist.drawGraphic(graphic)
+                let nameGraphics = layout.layoutNames(using: resolver)
+                nameGraphics.forEach { graphic in
+                    namesArtist.drawGraphic(graphic)
                 }
             }
         }
